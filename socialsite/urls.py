ec49f4ec4d3244c66ee0872415cc14b9b447d477
Abdullah Karagøz <47918976+STProgrammer@users.noreply.github.com>
2025-06-04 18:11:38 +0200

    Enhance login page with Bootstrap

--- socialsite/urls.py
+++ socialsite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from socialapp.forms import BootstrapAuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('socialapp.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(authentication_form=BootstrapAuthenticationForm), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
